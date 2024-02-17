09c01da20ae406681abd2f493aa200963d2fbf1b
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-02-17 16:43:04 +0530

    Version 1

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+
+def get_response(input_prompt):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input_prompt)
+    return response.text
+
+# Application Below
+
+st.set_page_config("CostLLM")
+st.header("Cost Predictor")
+
+prod_desc = st.text_input("Enter Product Description")
+complexity = st.selectbox('What is complexity of Project?', ('Low', 'Medium', 'High'))
+team_size = st.number_input("Enter Team Size")
+experience = st.selectbox('What is Team Experience?', ('Beginner', 'Intermediate', 'Expert'))
+llm_used = st.text_input("Which LLM is being Used")
+
+submit = st.button("Get Estimate")
+
+input_prompt= """
+You are an expert project manager and you have a great experience with LLMs, your new job is to predict the cost and time required for building a new product provided the following details
+        - Product Description = {prod_desc}
+        - Project complexity = {complexity}
+        - Team Size = {team_size}
+        - Team Experience = {experience}
+        - LLM to be used in product = {llm_used}
+
+        You have to return Estimated Cost in Rupees and Estimated Time in Hours in format 
+        Cost : 
+        Time :
+
+"""
+
+if submit:
+    response = get_response(input_prompt)
+    st.write(response)
+
+
